f76d58f4a05395f7971eed7cded5355e2b66a56f
Rama Adhitya Setiadi <ramaadhityasetiadi002@gmail.com>
2024-04-01 11:31:13 +0700

    revoke auth

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,7 +1,32 @@
 <x-layout-auth>
-    <main class="authentication-content mt-5">
-        <div class="container-fluid">
-            @slot('title', 'Register')
+    @slot('title', 'Registration')
+
+    <div class="card">
+        <div class="card-body">
+            <!-- Logo -->
+            <div class="app-brand justify-content-center mb-4 mt-2">
+                <a href="index.html" class="app-brand-link gap-2">
+                    <span class="app-brand-logo demo">
+                        <svg width="32" height="22" viewBox="0 0 32 22" fill="none"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" clip-rule="evenodd"
+                                d="M0.00172773 0V6.85398C0.00172773 6.85398 -0.133178 9.01207 1.98092 10.8388L13.6912 21.9964L19.7809 21.9181L18.8042 9.88248L16.4951 7.17289L9.23799 0H0.00172773Z"
+                                fill="#7367F0" />
+                            <path opacity="0.06" fill-rule="evenodd" clip-rule="evenodd"
+                                d="M7.69824 16.4364L12.5199 3.23696L16.5541 7.25596L7.69824 16.4364Z" fill="#161616" />
+                            <path opacity="0.06" fill-rule="evenodd" clip-rule="evenodd"
+                                d="M8.07751 15.9175L13.9419 4.63989L16.5849 7.28475L8.07751 15.9175Z" fill="#161616" />
+                            <path fill-rule="evenodd" clip-rule="evenodd"
+                                d="M7.77295 16.3566L23.6563 0H32V6.88383C32 6.88383 31.8262 9.17836 30.6591 10.4057L19.7824 22H13.6938L7.77295 16.3566Z"
+                                fill="#7367F0" />
+                        </svg>
+                    </span>
+                    <span class="app-brand-text demo text-body fw-bold ms-1">Ayasya Tech</span>
+                </a>
+            </div>
+            <!-- /Logo -->
+            <h4 class="mb-1 pt-2">Adventure starts here 🚀</h4>
+            <p class="mb-4">Make your app management easy and fun!</p>
 
             @if (session()->has('alert'))
                 <x-alert>
@@ -10,76 +35,70 @@
                 </x-alert>
             @endif
 
-            <div class="row">
-                <div class="col-12 col-lg-4 mx-auto">
-                    <div class="card shadow rounded-5 overflow-hidden">
-                        <div class="card-body p-4 p-sm-5">
-                            <h5 class="card-title">Register</h5>
-                            <p class="card-text mb-5">Hi, welcome to MPWA MD version.</p>
-                            <form class="form-body" action="{{ route('register') }}" method="POST">
-                                @csrf
-                                <div class="row g-3">
-                                    <div class="col-12">
-                                        <label for="username" class="form-label">Username</label>
-                                        <div class="ms-auto position-relative">
-                                            <div class="position-absolute top-50 translate-middle-y search-icon px-3">
-                                                <i class="bi bi-person-fill"></i>
-                                            </div>
-                                            <input type="text"
-                                                class="form-control radius-30 ps-5 {{ $errors->has('username') ? 'is-invalid' : '' }}"
-                                                id="username" name="username" placeholder="Username" required>
-                                        </div>
-                                        <p class="text-danger">
-                                            @error('username')
-                                                {{ $message }}
-                                            @enderror
-                                        </p>
-                                    </div>
-                                     <div class="col-12">
-                                        <label for="username" class="form-label">Email</label>
-                                        <div class="ms-auto position-relative">
-                                            <div class="position-absolute top-50 translate-middle-y search-icon px-3">
-                                                {{-- email icon --}}
-                                                <i class="bi bi-envelope-fill"></i>
-                                            </div>
-                                            <input type="text"
-                                                class="form-control radius-30 ps-5 {{ $errors->has('email') ? 'is-invalid' : '' }}"
-                                                id="email" name="email" placeholder="email" required>
-                                        </div>
-                                        <p class="text-danger">
-                                            @error('email')
-                                                {{ $message }}
-                                            @enderror
-                                        </p>
-                                    </div>
-                                    <div class="col-12">
-                                        <label for="password" class="form-label">Enter Password</label>
-                                        <div class="ms-auto position-relative">
-                                            <div class="position-absolute top-50 translate-middle-y search-icon px-3"><i
-                                                    class="bi bi-lock-fill"></i></div>
-                                            <input type="password" name="password" class="form-control radius-30 ps-5"
-                                                id="password" placeholder="Enter Password" required>
+            <form id="formAuthentication" class="mb-3" action="{{ route('register') }}" method="POST">
+                @csrf
 
-                                        </div>
-
-                                    </div>
-
-                                    <div class="col-12">
-                                        <div class="d-grid">
-                                            <button type="submit" class="btn btn-primary radius-30">Register</button>
-                                        </div>
-                                    </div>
-                                    <div class="col-12">
-                                        <p class="mb-0">Already have account? <a
-                                                href="{{ route('login') }}">Sign in
-                                                here</a></p>
-                                    </div>
-                                </div>
-                            </form>
-                        </div>
+                <div class="mb-3">
+                    <label for="username" class="form-label">Username</label>
+                    <input type="text" class="form-control {{ $errors->has('username') ? 'is-invalid' : '' }}"
+                        id="username" name="username" placeholder="Enter your username" autofocus required />
+                    <p class="text-danger">
+                        @error('username')
+                            {{ $message }}
+                        @enderror
+                    </p>
+                </div>
+                <div class="mb-3">
+                    <label for="email" class="form-label">Email</label>
+                    <input type="email" class="form-control {{ $errors->has('email') ? 'is-invalid' : '' }}"
+                        id="email" name="email" placeholder="Enter your email" autofocus required />
+                    <p class="text-danger">
+                        @error('email')
+                            {{ $message }}
+                        @enderror
+                    </p>
+                </div>
+                <div class="mb-3 form-password-toggle">
+                    <div class="d-flex justify-content-between">
+                        <label class="form-label" for="password">Password</label>
+                    </div>
+                    <div class="input-group input-group-merge">
+                        <input type="password" id="password" class="form-control" name="password"
+                            placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;"
+                            aria-describedby="password" required />
+                        <span class="input-group-text cursor-pointer"><i class="ti ti-eye-off"></i></span>
                     </div>
                 </div>
+                <div class="mb-3">
+                    <button class="btn btn-primary d-grid w-100" type="submit">Sign up</button>
+                </div>
+            </form>
+
+            <p class="text-center">
+                <span>Already have account?</span>
+                <a href="{{ route('login') }}">
+                    <span>Sign in here</span>
+                </a>
+            </p>
+
+            <div class="divider my-4">
+                <div class="divider-text">or</div>
+            </div>
+
+            <div class="d-flex justify-content-center">
+                <a href="javascript:;" class="btn btn-icon btn-label-facebook me-3">
+                    <i class="tf-icons fa-brands fa-facebook-f fs-5"></i>
+                </a>
+
+                <a href="javascript:;" class="btn btn-icon btn-label-google-plus me-3">
+                    <i class="tf-icons fa-brands fa-google fs-5"></i>
+                </a>
+
+                <a href="javascript:;" class="btn btn-icon btn-label-twitter">
+                    <i class="tf-icons fa-brands fa-twitter fs-5"></i>
+                </a>
             </div>
         </div>
-      
+    </div>
+
 </x-layout-auth>
--- resources/views/components/layout-auth.blade.php
+++ resources/views/components/layout-auth.blade.php
@@ -1,5 +1,9 @@
+<!doctype html>
+
+<html lang="en" class="light-style layout-wide customizer-hide" dir="ltr" data-theme="theme-default"
+    data-assets-path="{{ asset('assets/') }}" data-template="vertical-menu-template-no-customizer">
+
 <head>
-   
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -11,31 +15,88 @@
     <!-- The above 6 meta tags *must* come first in the head; any other head content must come *after* these tags -->
 
     <!-- Title -->
-    <title>{{ $title }} | MPWA MD version</title>
+    <title>{{ $title }} | Ayasya Tech</title>
 
-    <!-- Styles -->
-    <link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet" />
-    <link href="{{ asset('assets/css/bootstrap-extended.css') }}" rel="stylesheet" />
-    <link href="{{ asset('assets/css/style.css') }}" rel="stylesheet" />
-    <link href="{{ asset('assets/css/icons.css') }}" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
-    <script src="{{ asset('assets/js/jquery.min.js') }}"></script>
+    <!-- Favicon -->
+    <link rel="icon" type="image/x-icon" href="{{ asset('assets/new-template/img/favicon/favicon.ico') }}" />
 
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+        href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&ampdisplay=swap"
+        rel="stylesheet" />
 
+    <!-- Icons -->
+    <link rel="stylesheet" href="{{ asset('assets/new-template/vendor/fonts/fontawesome.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/new-template/vendor/fonts/tabler-icons.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/new-template/vendor/fonts/flag-icons.css') }}" />
 
+    <!-- Core CSS -->
+    <link rel="stylesheet" href="{{ asset('assets/new-template/vendor/css/rtl/core.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/new-template/vendor/css/rtl/theme-default.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/new-template/css/demo.css') }}" />
 
+    <!-- Vendors CSS -->
+    <link rel="stylesheet" href="{{ asset('assets/new-template/vendor/libs/node-waves/node-waves.css') }}" />
+    <link rel="stylesheet"
+        href="{{ asset('assets/new-template/vendor/libs/perfect-scrollbar/perfect-scrollbar.css') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/new-template/vendor/libs/typeahead-js/typeahead.css') }}" />
+    <!-- Vendor -->
+    <link rel="stylesheet"
+        href="{{ asset('assets/new-template/vendor/libs/@form-validation/form-validation.css') }}" />
+
+    <!-- Page CSS -->
+    <!-- Page -->
+    <link rel="stylesheet" href="{{ asset('assets/new-template/vendor/css/pages/page-auth.css') }}" />
+
+    <!-- Helpers -->
+    <script src="{{ asset('assets/new-template/vendor/js/helpers.js') }}"></script>
+    <!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
+    <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
+    <script src="{{ asset('assets/new-template/js/config.js') }}"></script>
 </head>
 
-<body class="bg-login">
-    <div class="wrapper">
+<body>
+    <!-- Content -->
 
-        {{ $slot }}
+    <div class="container-xxl">
+        <div class="authentication-wrapper authentication-basic container-p-y">
+            <div class="authentication-inner py-4">
+                <!-- body -->
+                {{ $slot }}
+                <!-- /body -->
+            </div>
+        </div>
     </div>
 
-</body>
+    <!-- / Content -->
 
-<script src="{{ asset('assets/js/pace.min.js') }}"></script>
+    <!-- Core JS -->
+    <!-- build:js assets/vendor/js/core.js -->
+
+    <script src="{{ asset('assets/new-template/vendor/libs/jquery/jquery.js') }}"></script>
+    <script src="{{ asset('assets/new-template/vendor/libs/popper/popper.js') }}"></script>
+    <script src="{{ asset('assets/new-template/vendor/js/bootstrap.js') }}"></script>
+    <script src="{{ asset('assets/new-template/vendor/libs/node-waves/node-waves.js') }}"></script>
+    <script src="{{ asset('assets/new-template/vendor/libs/perfect-scrollbar/perfect-scrollbar.js') }}"></script>
+    <script src="{{ asset('assets/new-template/vendor/libs/hammer/hammer.js') }}"></script>
+    <script src="{{ asset('assets/new-template/vendor/libs/i18n/i18n.js') }}"></script>
+    <script src="{{ asset('assets/new-template/vendor/libs/typeahead-js/typeahead.js') }}"></script>
+    <script src="{{ asset('assets/new-template/vendor/js/menu.js') }}"></script>
+
+    <!-- endbuild -->
+
+    <!-- Vendors JS -->
+    <script src="{{ asset('assets/new-template/vendor/libs/@form-validation/popular.js') }}"></script>
+    <script src="{{ asset('assets/new-template/vendor/libs/@form-validation/bootstrap5.js') }}"></script>
+    <script src="{{ asset('assets/new-template/vendor/libs/@form-validation/auto-focus.js') }}"></script>
+
+    <!-- Main JS -->
+    <script src="{{ asset('assets/new-template/js/main.js') }}"></script>
+
+    <!-- Page JS -->
+    <script src="{{ asset('assets/new-template/js/pages-auth.js') }}"></script>
 </body>
 
 </html>
